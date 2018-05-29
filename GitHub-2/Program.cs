daef7eacb05f22cfed2b1fdfbc2e75c590829294
Oleg <b020698@mail.ru>
2018-05-29 16:56:36 +0300

    GitHub 2

---
+++ GitHub-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GitHub cloud.");
+            Console.ReadKey();
+        }
+    }
+}
